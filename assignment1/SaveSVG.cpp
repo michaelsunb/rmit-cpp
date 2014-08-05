4bd3a271b52065b7521b22700b737fb85c9656e3
Michealsun Baluyos <michael.baluyos@gmail.com>
2014-08-06 02:06:37 +1000

    SaveSVG method now creates a file with correct values

--- assignment1/SaveSVG.cpp
+++ assignment1/SaveSVG.cpp
@@ -12,22 +12,30 @@ using namespace std;
 
 void Maze::SaveSVG(char *fileName)
 {
-	// TODO
-	cout << "\n--ss " << flush;
-	cout << fileName << endl;
-
-	cout << "svg width: " << this->width << endl;
-	cout << "svg height: " << this->height << endl;
-	cout << "svg number of lines: " << this->numOfLInes << endl;
-	cout << sizeof(this->x1) << endl;
+	ofstream svgFile(fileName, ofstream::out);
+	svgFile << "<svg viewBox='0 0 1 1' width='500' height='500' "
+			"xmlns='http://www.w3.org/2000/svg'>\n"
+			"<rect width='1' height='1' style='fill: black' />" << endl;
 
 	for(int ip = 0; ip < this->numOfLInes  ; ip++)
 	{
-		cout << ip << "\tx1: " << x1[ip] << "\t" << flush;
-		cout << "\ty1: " << y1[ip] << "\t" << flush;
-		cout << "\tx2: " << x2[ip] << "\t" << flush;
-		cout << "\ty2: " << y2[ip] << "\t" << endl;
+		/*
+		 * We want to cast the x,y arrays to float
+		 * before we divide by the width,height.
+		 *
+		 * Use float to reduce memory consumption
+		 */
+		svgFile << "<line stroke='white' stroke-width='0.005'"
+				" x1='" << float(x1[ip])/width << "'"
+				" y1='" << float(y1[ip])/height << "'"
+				" x2='" << float(x2[ip])/width << "'"
+				" y2='" << float(y2[ip])/height << "'/>" << endl;
 	}
+
+	svgFile << "</svg>" << flush;
+	svgFile.close();
+
+	cout << "Successfully created " << fileName << "!" << endl;
 }
 
 
