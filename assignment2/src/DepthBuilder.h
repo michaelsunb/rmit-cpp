ba5c3d257c321a9498938a6b5809d6247af962c8
Michealsun Baluyos <michael.baluyos@gmail.com>
2014-10-03 18:35:53 +1000

    move files to src and fixed makefile

---
+++ assignment2/src/DepthBuilder.h
@@ -0,0 +1,73 @@
+/*
+ * DepthBuilder.h
+ *
+ *  Created on: 16 Aug 2014
+ *      Author: Michaelsun Baluyos
+ *       Email: s3110401@student.rmit.edu.au
+ */
+
+#ifndef DEPTHBUILDER_H_
+#define DEPTHBUILDER_H_
+
+
+#include "Assignment2.h"
+#include "Maze.h"
+#include "Cell.h"
+#include <random>
+
+class DepthBuilder
+{
+private:
+	int width = 0;
+	int height = 0;
+	Maze* buildMaze;
+
+	/**
+	 * enum of direction indicating that
+	 * x increment go east
+	 * y increment go north
+	 * x decrement go west
+	 * y decrement go south
+	 */
+	enum DIR {xIncrementGoEast,
+		yIncrementGoNorth,
+		xDecrementGoWest,
+		yDecrementGoSouth};
+
+	/**
+	 * To store which allowed direction
+	 */
+	std::vector<DIR> allowableNeighbours;
+	int currentX = 0;
+	int currentY = 0;
+
+	/**
+	 * Private methods because why do
+	 * other classes need to access
+	 * these methods
+	 *
+	 * CheckNeighbours to check the
+	 * current stack's neighbours
+	 * and set them in allowableNeighbours
+	 * vector
+	 */
+	void checkNeighbours();
+
+	/**
+	 * Based on the non-visited neighhbours
+	 * choose a random neighbour to go to
+	 */
+	void chooseRandomNeighbour(std::mt19937 &rng);
+public:
+
+	/**
+	 * Build method as stated in lab 4
+	 * For object orientation
+	 * Used to generate a new maze
+	 */
+	void build(Maze &maze,int width, int height,int seed);
+};
+
+
+
+#endif /* DEPTHBUILDER_H_ */
---
+++ assignment2/src/Maze/loadBinary.cpp
@@ -0,0 +1,140 @@
+/*
+ * LoadBinary.cpp
+ *
+ *  Created on: 29 Jul 2014
+ *      Author: Michaelsun Baluyos
+ */
+
+#include "../Maze.h"
+
+const int widthBinary = 0;
+const int heightBinary = 1;
+const int numberOfEdgesBinary = 2;
+const int everyForthBinary = 4;
+
+using namespace std;
+
+void Maze::loadBinary(char *fileName)
+{
+	cout << "Loading maze: " << fileName << endl;
+
+	ifstream dataFile(fileName,ios::binary);
+
+	if(!dataFile.is_open())
+	{
+		throw "Maze binary file was not found";
+	}
+
+	/**
+	 * Use binaryElement to get the first
+	 * three binary numbers.
+	 */
+	int binaryElement = 0;
+	int xyBinaryElement = 0;
+
+	int x = 0;
+
+	while(!dataFile.eof())
+	{
+		/**
+		 * Read the binary file and
+		 * set the read value into x
+		 */
+		dataFile.read((char*)&x,sizeof(int));
+
+		/**
+		 * None of the binary values should
+		 * be less than zero. None!
+		 */
+		if(x < 0)
+		{
+			throw "Invalid binary file";
+		}
+
+		if(binaryElement == widthBinary)
+		{
+			this->setWidth(x);
+		}
+		else if(binaryElement == heightBinary)
+		{
+			this->setHeight(x);
+		}
+		else if(binaryElement == numberOfEdgesBinary)
+		{
+			if((width * height) != x)
+			{
+				throw "Invalid binary file";
+			}
+			/**
+			 * Initialise edge vector
+			 * Create an array size based on
+			 * the number of edges
+			 */
+			this->setNumOfEdges(x);
+			eArray.resize(this->numOfEdges);
+		}
+		else
+		{
+			/**
+			 * After setting the width, height and number
+			 * of Edges, we now set the x1,y1,x2,y2 values
+			 *
+			 * So we minus in the order u see below and
+			 * divide and get the remainder to get the
+			 * correct binary number.
+			 * [w][h][E]
+			 * [0][1][2][ (3 - 3) / 4 r 0 ]
+			 * [0][1][2][3][ 4 / 4 r 0 ]
+			 * [0][1][2][3][4][ (5 - 1) / 4 r 0 ]
+			 * [0][1][2][3][4][5][ (6 - 2) /4 r 0 ]
+			 *
+			 * Start over again...
+			 * [0][1][2][3][4][5][6][ (7 - 3) /4 r 0 ]
+			 */
+			if(xyBinaryElement < numOfEdges)
+			{
+				if(((binaryElement - 3) % everyForthBinary) == 0)
+				{
+					eArray[xyBinaryElement].x1 = x;
+				}
+				if(((binaryElement) % everyForthBinary) == 0)
+				{
+					eArray[xyBinaryElement].y1 = x;
+				}
+				if(((binaryElement - 1) % everyForthBinary) == 0)
+				{
+					eArray[xyBinaryElement].x2 = x;
+				}
+				if(((binaryElement - 2) % everyForthBinary) == 0)
+				{
+					eArray[xyBinaryElement].y2 = x;
+				}
+			}
+
+			/**
+			 * Need this outside from above
+			 * if(((binaryElement - 2) % everyForthBinary) == 0)
+			 * because if the binary file is greater
+			 * than the size of the vector, then it
+			 * will cause an error
+			 */
+			if(((binaryElement - 2) % everyForthBinary) == 0)
+			{
+				xyBinaryElement++;
+			}
+
+			if(xyBinaryElement > numOfEdges)
+			{
+				throw "Invalid binary file";
+			}
+		}
+		binaryElement++;
+	}
+	dataFile.close();
+
+	if(xyBinaryElement != this->numOfEdges)
+	{
+		throw "Invalid binary file";
+	}
+}
+
